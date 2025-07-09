dbd2a238ee447ce31efcefeb4d9d1b518c4b9a30
formation1 <kirisiri04@gmail.com>
2025-07-09 17:47:07 +0200

    Update conftest.py

--- taskmanager-api/tests/integration/conftest.py
+++ taskmanager-api/tests/integration/conftest.py
@@ -30,6 +30,6 @@ async def setup_test_db():
 # ✅ Provide a test HTTP client using FastAPI's ASGI app
 @pytest_asyncio.fixture
 async def client():
-    transport = ASGITransport(app=app, lifespan="on")
+    transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         yield ac
